5692191ad822c29361225122d3c8d2ebdbeffc12
Volodymyr Zavalskyi <vovazav172839@gmail.com>
2020-08-08 22:52:50 +0300

    Add Triangle to main class

--- src/main/java/yurkovskiy/com/App.java
+++ src/main/java/yurkovskiy/com/App.java
@@ -2,12 +2,13 @@ package yurkovskiy.com;
 
 import yurkovskiy.com.shapes.base.Circle;
 import yurkovskiy.com.shapes.base.Rectangle;
+import yurkovskiy.com.shapes.base.Triangle;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
@@ -23,6 +24,8 @@ public class App
         System.out.println("The length of Circle is: "+ C.length());
         System.out.println("The square of Circle is: "+ C.square());
 
-
+        Triangle T = new Triangle(5,5,10);
+        System.out.println("The length of Triangle is: "+ C.length());
+        System.out.println("The square of Triangle is: "+ C.square());
     }
 }
