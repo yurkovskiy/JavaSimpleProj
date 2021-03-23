9c57b872239604be325c6704a4031f3e8b7388ec
Yuriy Bezgachnyuk <yurkovskiy@gmail.com>
2021-03-23 14:27:49 +0200

    Added new tests

--- src/test/java/yurkovskiy/com/shapes/base/RectangleTest.java
+++ src/test/java/yurkovskiy/com/shapes/base/RectangleTest.java
@@ -6,26 +6,32 @@ import junit.framework.TestSuite;
 
 public class RectangleTest extends TestCase {
 
-    public RectangleTest(String testName)
-    {
+    public RectangleTest(String testName) {
         super(testName);
     }
 
-    public static Test suite()
-    {
+    public static Test suite() {
         return new TestSuite(RectangleTest.class);
     }
 
-    public void testRectangleLength()
-    {
+    public void testRectangleLength() {
         Rectangle R = new Rectangle(10.0, 10.0);
         assertEquals(40.0, R.length(), 0.0);
     }
 
-        public void testRectangleSquare()
-        {
-            Rectangle R = new Rectangle(10.0, 10.0);
-            assertEquals(100, R.square(), 0.0);
-        }
+    public void testRectangleSquare() {
+        Rectangle R = new Rectangle(10.0, 10.0);
+        assertEquals(100, R.square(), 0.0);
+    }
+
+    public void testRectangleLength2() {
+        Rectangle R = new Rectangle(5.0, 2.0);
+        assertEquals(14.0, R.length(), 0.0);
+    }
+
+    public void testRectangleSquare2() {
+        Rectangle R = new Rectangle(5.0, 2.0);
+        assertEquals(10, R.square(), 0.0);
+    }
 
 }
